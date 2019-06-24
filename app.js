25d6b2f197b6370a69d73b5b4545b203eba3750f
Bacchus <47571329+BacchusJ@users.noreply.github.com>
2019-06-24 18:44:31 -0500

    added functions at the bottom
    game still not calling the function when it starts

---
+++ app.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  let guess = 100;
+
+// Challenge 1: Creating a random number.
+
+  function generateRandomNumber() {
+    return Math.floor((Math.random() * 100) + 1);
+}
+
+
+// Challenge 2: Assigning a variable.
+
+  let ranNum = generateRandomNumber();
+  console.log(ranNum)
+
+
+
+// Challenge 3: Using if/else and value comparison
+
+
+  function checkGuess(guess) {
+
+    // Write your code here
+    if (guess === ranNum)  {
+      alert("You are correct");
+       }
+    else{
+      alert("Keep on trying");
+    }
+    
+
+  }
+
+  checkGuess(ranNum);
+
+})
+
+
+
+
+
